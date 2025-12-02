d9323759216f86bc6831e6779384488723f98079
Alicia <94490665+acaruso369@users.noreply.github.com>
2025-12-02 20:15:10 +0000

    a lot of changes

--- src/script.js
+++ src/script.js
@@ -11,3 +11,33 @@ hamburger.addEventListener("click", () => {
     }
 });
 
+// Cocktail Ingredients Dataset 
+fetch("data/final_cocktails.csv")
+  .then(response => response.text())
+  .then(csv => {
+      console.log(csv); // raw CSV text
+  })
+  .catch(err => console.error("Error reading file:", err));
+
+
+// Age Verification Popup
+document.addEventListener("DOMContentLoaded", () => {
+  const popup = document.getElementById("age-popup");
+  const yesBtn = document.getElementById("yesBtn");
+  const noBtn = document.getElementById("noBtn");
+
+  // If user already confirmed, skip popup
+  if (localStorage.getItem("ageVerified") === "true") {
+    popup.style.display = "none";
+  }
+
+  yesBtn.addEventListener("click", () => {
+    localStorage.setItem("ageVerified", "true");
+    popup.style.display = "none";
+  });
+
+  noBtn.addEventListener("click", () => {
+    alert("You must be 21 or older to enter this site.");
+    window.location.href = "https://www.google.com";
+  });
+});
